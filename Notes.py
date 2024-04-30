f96aaf89f91446db004f2c66f416146a8105e4aa
Vladimir <buyanov.vv@yandex.ru>
2024-04-30 17:28:35 +0700

    Шаблон программы

---
+++ Notes.py
@@ -0,0 +1,41 @@
+# Загружаем заметки из файла
+def load_notes():
+    pass
+
+# Сохраняем заметки в файл
+def save_note(data):
+    pass
+
+# Печать заметок
+def printNotes(data, keys):
+    pass
+
+# Запрашиваем у пользователя данные
+def input_title():
+    pass
+
+def input_text():
+    pass
+
+# Добавляем новую заметку
+def add_note(data: dict):
+    pass
+
+# Редактируем заметку
+def edit_note(data):
+    pass
+
+# Удаляем заметку
+def del_note(data):
+    pass
+
+# Поиск заметки по дате
+def dateSearch_note(data):
+    pass
+
+# Интерфейс пользователя
+def interface():
+    pass
+
+if __name__ == '__main__':
+    interface()
